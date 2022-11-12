ddea14adf05a553cae89e3873f8d814ab5e6dd38
erickariasec <erickarias1999@hotmail.com>
2022-11-12 10:09:25 -0500

    Adding Promo and styling

---
+++ src/components/Promo.jsx
@@ -0,0 +1,13 @@
+import "./Promo.css"
+
+export const Promo = () => (
+    <>
+        <section className="promo-header">
+            <div className="content">
+                <h2>REBAJAS!</h2>
+                <h2>20% OFF!</h2>
+                <h2>REBAJAS!</h2>
+            </div>
+        </section>
+        </>
+);
