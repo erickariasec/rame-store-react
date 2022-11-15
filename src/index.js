468011aa4c75bf983693bb611c5db41724d6c101
erickariasec <erickarias1999@hotmail.com>
2022-11-14 22:10:11 -0500

    Uninstalling NextUI React

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { NextUIProvider } from '@nextui-org/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <NextUIProvider>
-            <App />
-        </NextUIProvider>
+        <App />
     </React.StrictMode>
 );
