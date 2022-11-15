57beefb35235d9c0313b25c8a3efd17277bf7153
erickariasec <erickarias1999@hotmail.com>
2022-11-14 22:00:59 -0500

    Adding Cards Container (for all products)

---
+++ src/components/CardsContainer.jsx
@@ -0,0 +1,12 @@
+import "./CardsContainer.css"
+import { ProductCard } from "./ProductCard";
+
+export const CardsContainer = () => (
+    <>
+        <main className="main">
+            <section className="cards">
+                <ProductCard />
+            </section>
+        </main>
+    </>
+);
