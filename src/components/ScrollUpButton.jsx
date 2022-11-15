8ea047945054b0d0b30265db926926f5a606bc81
erickariasec <erickarias1999@hotmail.com>
2022-11-14 22:01:54 -0500

    Adding Scroll Up Button structure and styling

---
+++ src/components/ScrollUpButton.jsx
@@ -0,0 +1,9 @@
+import "./ScrollUpButton.css"
+
+export const ScrollUpButton = () => (
+    <>
+        <div class="scroll-up-btn">
+            <i class="fas fa-angle-up"></i>
+        </div>
+    </>
+);
