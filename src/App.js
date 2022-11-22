07540853097703f504c0d5ce8c461c3a0397343a
erickariasec <erickarias1999@hotmail.com>
2022-11-22 00:52:54 -0500

    Implementing Azure Authentication for all the App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import "./App.css";
-import { CardsContainer } from "./components/CardsContainer";
-import { Navbar } from "./components/Navbar";
-// import { Promo } from "./components/Promo";
-
 import "swiper/css/bundle";
 import "./components/GallerySwiper.css";
-import { GallerySwiper } from "./components/GallerySwiper";
-import { ScrollUpButton } from "./components/ScrollUpButton";
+
+import { PageLayout } from "./components/PageLayout";
+import {
+    AuthenticatedTemplate,
+    UnauthenticatedTemplate,
+    useMsal,
+} from "@azure/msal-react";
+
+function ProfileContent() {
+    const { accounts } = useMsal();
+
+    const name = accounts[0] && accounts[0].name;
+
+    return (
+        <>
+            <h5 className="welcome-msg">Bienvenido/a {name}</h5>
+        </>
+    );
+};
 
 function App() {
     
     return (
         <div className="body">
-            <ScrollUpButton />
-            {/* <Promo /> */}
-            <Navbar />
-            <GallerySwiper />
-            <CardsContainer />
+            <PageLayout>
+                <AuthenticatedTemplate>
+                    <ProfileContent />
+                </AuthenticatedTemplate>
+                <UnauthenticatedTemplate>
+                    <p className="welcome-msg"></p>
+                </UnauthenticatedTemplate>
+            </PageLayout>
         </div>
     );
 }
