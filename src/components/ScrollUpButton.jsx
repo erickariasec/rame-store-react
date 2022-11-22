775e91eec730e47d2c9c8e1ffcc77660701ad49c
erickariasec <erickarias1999@hotmail.com>
2022-11-22 00:48:22 -0500

    Changing from class to className at ScrollUpButton

--- src/components/ScrollUpButton.jsx
+++ src/components/ScrollUpButton.jsx
@@ -2,8 +2,8 @@ import "./ScrollUpButton.css"
 
 export const ScrollUpButton = () => (
     <>
-        <div class="scroll-up-btn">
-            <i class="fas fa-angle-up"></i>
+        <div className="scroll-up-btn">
+            <i className="fas fa-angle-up"></i>
         </div>
     </>
 );
